a21d03b1ea4b8d314a6b4d73508a7151db121282
Mubu81 <murat.budak@koeln.de>
2021-01-03 11:06:50 +0100

    git

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-
 import requests
 from flask import Flask, render_template, request, url_for, redirect, make_response
 from models import db, Kundendaten
+
 app = Flask(__name__)
 
 db.create_all()
 
+
 @app.route('/')
 def kunden():
     anfragen_db = db.query(Kundendaten).all()
-    return render_template("kundendaten.html",anfragen=anfragen_db)
+    return render_template("kundendaten.html", anfragen=anfragen_db)
+
 
 @app.route("/kundendaten", methods=["POST"])
 def infos():
@@ -22,11 +24,12 @@ def infos():
     email = request.form.get("email")
     tel = request.form.get("tel")
     new_request = Kundendaten(vorname=vorname, nachname=nachname, strasse=strasse, hausnummer=hausnummer,
-                    plz=plz, stadt=stadt, email=email,tel=tel)
+                              plz=plz, stadt=stadt, email=email, tel=tel)
     db.add(new_request)
     db.commit()
     anfragen_db = db.query(Kundendaten).all()
     return redirect(url_for("kunden"))
+
 
 @app.route('/delete', methods=['POST'])
 def delete():
