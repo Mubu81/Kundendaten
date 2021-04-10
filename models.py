dce4c189ac386904cbce70fe0281a48a7f9b6aa6
Murat Budak <murat.budak@koeln.de>
2021-04-10 02:32:47 +0200

    git

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "postgres://luejhxncqloyoe:e38f1cb714f6727eb4448563beaea4aa089e03b4ff4d3fd52b4cc6d4c590b53d@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/den2l0igh4j0kh"))  # this connects to a database either on Heroku or on localhost
+db = SQLAlchemy(os.getenv("DATABASE_URL", "postgresql://luejhxncqloyoe:e38f1cb714f6727eb4448563beaea4aa089e03b4ff4d3fd52b4cc6d4c590b53d@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/den2l0igh4j0kh"))  # this connects to a database either on Heroku or on localhost
 
 
 class Kundendaten(db.Model):
