2afe655485997c9e2db1e310e2c8b8a12ad37f33
Murat Budak <murat.budak@koeln.de>
2021-04-25 12:03:14 +0200

    git

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 #import os
-#from sqla_wrapper import SQLAlchemy
+from sqla_wrapper import SQLAlchemy
 import os
 import psycopg2
 
-db = os.environ['DATABASE_URL']
+#db = os.environ['DATABASE_URL']
 
+db = SQLAlchemy(os.getenv("DATABASE_URL")) # this connects to a database either on Heroku or on localhost
 conn = psycopg2.connect(db, sslmode='require')
 
-#db = SQLAlchemy(os.getenv("DATABASE_URL", "postgresql://luejhxncqloyoe:e38f1cb714f6727eb4448563beaea4aa089e03b4ff4d3fd52b4cc6d4c590b53d@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/den2l0igh4j0kh"))  # this connects to a database either on Heroku or on localhost
 #db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))  # this connects to a database either on Heroku or on localhost
 
 class Kundendaten(db.Model):
