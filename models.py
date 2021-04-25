1ea40a3deaf76d7fde09b9062940e6d485260233
Murat Budak <murat.budak@koeln.de>
2021-04-25 11:41:10 +0200

    git

--- models.py
+++ models.py
@@ -3,9 +3,9 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['postgres://luejhxncqloyoe:e38f1cb714f6727eb4448563beaea4aa089e03b4ff4d3fd52b4cc6d4c590b53d@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/den2l0igh4j0kh"))']
+db = os.environ['postgres://luejhxncqloyoe:e38f1cb714f6727eb4448563beaea4aa089e03b4ff4d3fd52b4cc6d4c590b53d@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/den2l0igh4j0kh"))']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(db, sslmode='require')
 
 #db = SQLAlchemy(os.getenv("DATABASE_URL", "postgresql://luejhxncqloyoe:e38f1cb714f6727eb4448563beaea4aa089e03b4ff4d3fd52b4cc6d4c590b53d@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/den2l0igh4j0kh"))  # this connects to a database either on Heroku or on localhost
 #db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))  # this connects to a database either on Heroku or on localhost
